1e5b79c5aa11a9fe5655a470b6459091d9b13331
herzaparam <herzaparam@gmail.com>
2023-12-28 15:30:53 +0700

    configure vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      server: {
+        port: 3000,
+        strictPort: true,
+        open: true
+      },
+      plugins: [react()]
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+      plugins: [react()]
+    }
+  }
 })
