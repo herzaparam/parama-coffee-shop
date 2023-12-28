ddc2ab2d4f3406c7b733fa5d4f0e3115e2661fa0
herzaparam <herzaparam@gmail.com>
2023-12-28 17:14:07 +0700

    create button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
-function Button({ className, onClick, style, label, ...props }) {
+function Button({ className, onClick, style, label, type, ...props }) {
+  const btnClass = classNames(
+    "rounded-2xl w-full p-4 font-bold text-lg outline-none transform transition duration-200 hover:scale-105",
+    {
+      [`bg-primary text-white hover:bg-primary-dark`]: style === "primary",
+      [`bg-secondary text-primary hover:bg-secondary-dark hover:text-primary-dark`]:
+        style === "secondary",
+    },
+    className
+  );
+
   return (
-    <button className={`${className}`} onClick={onClick} {...props}>
+    <button className={btnClass} onClick={onClick} {...props}>
       {label}
     </button>
   );
@@ -11,6 +22,7 @@ function Button({ className, onClick, style, label, ...props }) {
 
 Button.propTypes = {
   className: PropTypes.string,
+  type: PropTypes.string,
   style: PropTypes.string,
   label: PropTypes.string.isRequired,
   onClick: PropTypes.func,
@@ -18,7 +30,8 @@ Button.propTypes = {
 
 Button.defaultProps = {
   className: "",
-  style: {},
+  style: "primary",
+  type: "button",
   onClick: () => {},
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,25 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
+    colors: {
+      primary: {
+        light: "#8E5E43",
+        DEFAULT: "#6A4029",
+        dark: "#512E1F",
+      },
+      secondary: {
+        light: "#FFD655",
+        DEFAULT: "#FFBA33",
+        dark: "#E6A22A",
+      },
+      tertiary: {
+        light: "#162240",
+        DEFAULT: "#0B132A",
+        dark: "#080F1D",
+      },
+      black: "#000000",
+      white: "#FFFFFF",
+    },
     extend: {
       backgroundImage: {
         'coffee': "url('/src/assets/images/bg_landing.webp')"
